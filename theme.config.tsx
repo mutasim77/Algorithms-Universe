f6c4e276e4b15675f82a54149c1eab7cbf5fe7e6
 <96326525+mutasim77@users.noreply.github.com>
2023-09-24 19:21:15 +0600

    some changes

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,57 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import React from 'react';
+import { DocsThemeConfig } from 'nextra-theme-docs';
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>Mutasim</span>,
   project: {
     link: 'https://github.com/shuding/nextra-docs-template',
   },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
   footer: {
-    text: 'Nextra Docs Template',
+    text: (
+      <div className="flex w-full flex-col items-center sm:items-start">
+        <div>
+          <a
+            className="flex items-center gap-1 text-current"
+            target="_blank"
+            rel="noopener noreferrer"
+            title="vercel.com homepage"
+            href="https://vercel.com?utm_source=nextra.site"
+          >
+            <span className='text-1xl'>Algos</span>
+          </a>
+        </div>
+        <p className="mt-6 text-xs">
+          Copyright © {new Date().getFullYear()} Mutasim
+        </p>
+        <p>All rights reserved.</p>
+      </div>
+    )
+  },
+  toc: {
+    backToTop: true
+  },
+  banner: {
+    key: 'github',
+    text: (
+      <a href="https://github.com" target="_blank">
+        Grant a Star ⭐ on Github  – It&apos;s for You ❤️
+      </a>
+    )
+  },
+  navbar: {
+    extraContent: <></>
+  },
+  faviconGlyph: 'M',
+  sidebar: {
+    titleComponent({ title, type }) {
+      if (type === 'separator') {
+        return <span className="cursor-default">{title}</span>
+      }
+      return <>{title}</>
+    },
+    defaultMenuCollapseLevel: 1,
+    toggleButton: true
   },
 }
 
-export default config
+export default config;
