f69c83909014ee5e3342cfe581bad9d68dfd1003
 <96326525+mutasim77@users.noreply.github.com>
2023-09-25 12:53:57 +0600

    bug: fixed

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,35 +2,33 @@ import React from 'react';
 import { DocsThemeConfig } from 'nextra-theme-docs';
 
 const config: DocsThemeConfig = {
-  logo: <span>LOGO</span>,
+  logo: <span>AlgoUniverse</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/mutasim77/Algorithms-Universe',
   },
   footer: {
     text: (
-      <div className="flex w-full flex-col items-center sm:items-start">
-        <div>
-          <a
-            className="flex items-center gap-1 text-current"
-            target="_blank"
-            rel="noopener noreferrer"
-            title="vercel.com homepage"
-            href="https://vercel.com?utm_source=nextra.site"
-          >
-            <span className='text-1xl'>Algos</span>
-          </a>
-        </div>
-        <p className="mt-6 text-xs">
-          Copyright © {new Date().getFullYear()} Mutasim
+      <div className="flex w-full flex-col items-center">
+        <a
+          className="flex items-center gap-1 text-current"
+          rel="noopener noreferrer"
+          title="Algorithms Universe"
+          href="/"
+        >
+          <span className='text-1xl'>Algorithms Universe</span>
+        </a>
+        <p className="mt-6 text-xs text-center">
+          Copyright © 2023 - 2024 Mutasim
+          <br />
+          All rights reserved.
         </p>
-        <p>All rights reserved.</p>
       </div>
     )
   },
   banner: {
     key: 'github',
     text: (
-      <a href="https://github.com" target="_blank">
+      <a href="https://github.com/mutasim77/Algorithms-Universe" target="_blank">
         Grant a Star ⭐ on Github  – It&apos;s for You ❤️
       </a>
     )
