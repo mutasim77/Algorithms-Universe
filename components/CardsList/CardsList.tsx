7f9c8350a701b4164a2d88b3a7cd158691bbe161
 <96326525+mutasim77@users.noreply.github.com>
2023-10-04 09:52:31 +0600

    ✨ Changed: Separate component for Cards

--- components/CardsList/CardsList.tsx
+++ components/CardsList/CardsList.tsx
@@ -1,70 +1,75 @@
-import { Card, Cards } from 'nextra/components'
+import { Card } from 'nextra/components';
 
 interface cardsProps {
     title: string,
-    href: string
+    href: string,
 }
 
 const cardsInfo: cardsProps[] = [
     {
         title: 'Bubble Sort',
-        href: 'sorts/bubble-sort'
+        href: 'sorts/bubble-sort',
     },
     {
         title: 'Selection Sort',
-        href: 'sorts/selection-sort'
+        href: 'sorts/selection-sort',
     },
     {
         title: 'Insertion Sort',
-        href: 'sorts/insertion-sort'
+        href: 'sorts/insertion-sort',
     },
     {
         title: 'Merge Sort',
-        href: 'sorts/merge-sort'
+        href: 'sorts/merge-sort',
     },
     {
         title: 'Quick Sort',
-        href: 'sorts/quick-sort'
+        href: 'sorts/quick-sort',
     },
     {
         title: 'Heap Sort',
-        href: 'sorts/heap-sort'
+        href: 'sorts/heap-sort',
     },
     {
         title: 'Radix Sort',
-        href: 'sorts/radix-sort'
+        href: 'sorts/radix-sort',
     },
     {
         title: 'Bucket Sort',
-        href: 'sorts/bucket-sort'
+        href: 'sorts/bucket-sort',
     },
     {
         title: 'Counting Sort',
-        href: 'sorts/counting-sort'
+        href: 'sorts/counting-sort',
     },
     {
         title: 'Timsort Sort',
-        href: 'sorts/timsort'
+        href: 'sorts/timsort',
     },
     {
         title: 'Cocktail Shaker Sort',
-        href: 'sorts/cocktail-shaker-sort'
+        href: 'sorts/cocktail-shaker-sort',
     },
     {
         title: 'Gnome Sort',
-        href: 'sorts/gnome-sort'
+        href: 'sorts/gnome-sort',
     },
 ]
 
-const CardsList = () => {
-
+const CardsList = (): JSX.Element => {
     return (
         <>
-            {cardsInfo.map((item) => (
-                <Card
-                    title={item.title}
-                    href={item.href}
-                />
+            {cardsInfo.map((item, inx) => (
+                <>
+                    {/* Adding children and icon just to avoid ts error! */}
+                    <Card
+                        key={inx}
+                        children={<></>}
+                        title={item.title}
+                        href={item.href}
+                        icon={<></>}
+                    />
+                </>
             ))}
         </>
     )
